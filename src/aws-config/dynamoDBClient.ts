ef022fefb792f8a2b581f079ff338ad182d300d1
JiaJiey <jiajieyong91@gmail.com>
2024-05-01 16:40:25 +0800

    feat: added lib-dynamodb

--- src/aws-config/dynamoDBClient.ts
+++ src/aws-config/dynamoDBClient.ts
@@ -1,5 +1,7 @@
 import * as AWS from 'aws-sdk';
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 import 'dotenv/config';
 
 const { ENDPOINT_URL, REGION } = process.env;
@@ -10,3 +12,11 @@ export const dynamoDBClient = (): DocumentClient => {
     endpoint: ENDPOINT_URL,
   });
 };
+
+const dynamoClient = new DynamoDBClient({
+  region: REGION,
+  endpoint: ENDPOINT_URL,
+});
+
+export const client = DynamoDBDocumentClient.from(dynamoClient);
+export const DATA_TABLE = 'data';
