111af36e212fa348022d9a05821c769f49b17094
JiaJiey <jiajieyong91@gmail.com>
2024-05-03 11:43:11 +0800

    feat: get all teachers with students assigned to them

--- src/core/features/teacher/get-all-teachers.ts
+++ src/core/features/teacher/get-all-teachers.ts
@@ -7,8 +7,13 @@ import {
 } from '@nestjs/cqrs';
 import { client } from 'src/aws-config/dynamoDBClient';
 import { QueryCommand } from '@aws-sdk/lib-dynamodb';
+import { TEACHER_ID_PREFIX } from '../../utils/constants';
 
 const { TABLE_NAME } = process.env;
+interface ITeacher {
+  email: string;
+  students: string[];
+}
 
 class GetTeachersWithStudentsQuery {
   constructor() {}
@@ -28,7 +33,7 @@ class GetTeachersWithStudentsController {
 class GetTeachersWithStudentsHandler
   implements IQueryHandler<GetTeachersWithStudentsQuery>
 {
-  async getTeachersWithStudents(): Promise<Record<string, any>[]> {
+  async getAllTeachersEmail(): Promise<string[]> {
     const command = new QueryCommand({
       TableName: TABLE_NAME,
       IndexName: 'GSI1',
@@ -39,20 +44,57 @@ class GetTeachersWithStudentsHandler
       ExpressionAttributeValues: {
         ':pk': `TEACHER_INDEX`,
       },
+      ProjectionExpression: 'email',
     });
 
     try {
       const { Items = [] } = await client.send(command);
-      return Items;
+      const teacherEmails: string[] = Items.map((item) => item.email);
+      return teacherEmails;
     } catch (error) {
       console.error('Unable to query the table. Error:', error);
       throw error;
     }
   }
 
+  async listStudentsForTeachers(teacherEmails: string[]): Promise<ITeacher[]> {
+    const teacherwithStudentListCollection: ITeacher[] = [];
+    for (const email of teacherEmails) {
+      const command = new QueryCommand({
+        TableName: TABLE_NAME,
+        KeyConditionExpression: '#pk = :pk AND begins_with(#sk, :sk)',
+        ExpressionAttributeNames: {
+          '#pk': 'PK',
+          '#sk': 'SK',
+        },
+        ExpressionAttributeValues: {
+          ':pk': `${TEACHER_ID_PREFIX}${email}`,
+          ':sk': `STUDENT#`,
+        },
+        ProjectionExpression: 'studentEmail',
+      });
+
+      try {
+        const { Items = [] } = await client.send(command);
+        const teacher: ITeacher = {
+          email: email,
+          students: Items.map((item) => item.studentEmail),
+        };
+
+        teacherwithStudentListCollection.push(teacher);
+      } catch (error) {
+        console.error('Unable to query the table. Error:', error);
+        throw error;
+      }
+    }
+    return teacherwithStudentListCollection;
+  }
+
   async execute() {
-    const result = await this.getTeachersWithStudents();
-    return result;
+    const teacherEmails = await this.getAllTeachersEmail();
+    const result = await this.listStudentsForTeachers(teacherEmails);
+
+    return { teachers: result };
   }
 }
 
